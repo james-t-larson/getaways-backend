700c63e8d4e4eb890bc32cb4bc71f4f45016e84e
James Larson <jameslarson@ip-192-168-1-82.ec2.internal>
2024-03-28 13:10:14 -0700

    feat: add GraphQL query for listing places with filters and sorting

--- lib/getaways_web/schema/schema.ex
+++ lib/getaways_web/schema/schema.ex
@@ -12,6 +12,35 @@ defmodule GetawaysWeb.Schema.Schema do
         {:ok, Vacation.get_place_by_slug!(slug)}
       end
     end
+
+    @desc "Get a list of places"
+    field :places, list_of(:place) do
+       arg :limit, :integer
+       arg :order, type: :sort_order, default_value: :asc
+       arg :filter,
+       resolve fn _, args, _ ->
+        {:ok, Vacation.list_places(args)}
+       end
+    end
+  end
+
+  input_object :place_filter do
+    field :matching, :string
+    field :wifi, :boolean
+    field :pet_friendly, :boolean
+    field :pool, :boolean
+    field :guest_count, :integer
+    field :available_between, :date_range
+  end
+
+  input_object :date_range do
+    field :start_date, non_null(:date)
+    field :end_date, non_null(:date)
+  end
+
+  enum :sort_order do
+    value :asc
+    value :desc
   end
 
   object :place do

c405d1a88679edf7d813da30bc9227f78149c85f
James Larson <jameslarson@Jamess-MacBook-Pro.local>
2024-03-28 16:35:02 -0700

    chore: update bookings query functions and schema dataloader for bookings and accounts

--- lib/getaways/vacation.ex
+++ lib/getaways/vacation.ex
@@ -140,6 +140,18 @@ defmodule Getaways.Vacation do
     Dataloader.Ecto.new(Repo, query: &query/2)
   end
 
+  def query(Booking, %{scope: :place, limit: limit}) do
+    Booking
+    |> where(state: "reserved")
+    |> order_by([desc: :start_date])
+    |> limit(^limit)
+  end
+
+  def query(Booking, %{scope: :user}) do
+    Booking
+    |> order_by([asc: :start_date])
+  end
+
   def query(queryable, _) do
     queryable
   end
--- lib/getaways_web/schema/schema.ex
+++ lib/getaways_web/schema/schema.ex
@@ -55,7 +55,10 @@ defmodule GetawaysWeb.Schema.Schema do
     field :price_per_night, non_null(:decimal)
     field :image, non_null(:string)
     field :image_thumbnail, non_null(:string)
-    field :bookings, list_of(:booking), resolve: dataloader(Vacation)
+    field :bookings, list_of(:booking) do
+      arg :limit, type: :integer, default_value: 100
+      resolve dataloader(Vacation, :bookings, args: %{scope: :place})
+    end
     field :reviews, list_of(:review), resolve: dataloader(Vacation)
   end
 
@@ -66,7 +69,7 @@ defmodule GetawaysWeb.Schema.Schema do
     field :state, non_null(:string)
     field :total_price, non_null(:decimal)
     field :place, non_null(:place), resolve: dataloader(Vacation)
-    field :user, non_null(:user), resolve: dataloader(Vacation)
+    field :user, non_null(:user), resolve: dataloader(Accounts)
   end
 
   object :review do
@@ -74,23 +77,23 @@ defmodule GetawaysWeb.Schema.Schema do
     field :rating, non_null(:integer)
     field :comment, non_null(:string)
     field :inserted_at, non_null(:naive_datetime)
-    field :user, non_null(:user), resolve: dataloader(Vacation)
+    field :user, non_null(:user), resolve: dataloader(Accounts)
     field :place, non_null(:place), resolve: dataloader(Vacation)
   end
 
   object :user do
     field :username, non_null(:string)
     field :email, non_null(:string)
-    field :bookings, list_of(:booking), resolve: dataloader(Vacation)
+    field :bookings, list_of(:booking),
+     resolve: dataloader(Vacation, :bookings, args: %{scope: :user})
     field :review, list_of(:review), resolve: dataloader(Vacation)
   end
 
   def context(ctx) do
-    source = Dataloader.Ecto.new(Getaways.Repo)
-
     loader =
       Dataloader.new
-      |> Dataloader.add_source(Vacation, source)
+      |> Dataloader.add_source(Vacation, Vacation.datasource())
+      |> Dataloader.add_source(Accounts, Accounts.datasource())
 
     Map.put(ctx, :loader, loader)
   end
